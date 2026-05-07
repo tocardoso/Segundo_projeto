f805084767ffd9c32479c55deeb50343332bf3ea
tocardoso <toacardoso@gmail.com>
2026-05-07 15:25:22 +0100

    Adicionei o ficheiro numeros_iguais.py
    Adicionei o ficheiro numero_iguais.py que contem um programa em python que executa as seguintes tarefas: Solicita ao utilizador a introdução de dois números inteiros e, caso, sejam iguais, devolve a resposta "os numeros introduzidos são iguais. Caso os números sejam diferentes, não mostra qualquer mensagem.

---
+++ numeros_iguais.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  5 14:16:40 2026
+
+@author: Secretaria
+"""
+
+
+num1 = int(input("Introduz um número inteiro: "))
+num2 = int(input("Introduz outro número inteiro: "))
+
+
+if num1 == num2:
+    print("Os números são iguais.")
+else:
+    print("Os números introduzidos são diferentes.")
