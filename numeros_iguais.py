0ba0fc971a64d316970e20402c614b1675dc646b
tocardoso <toacardoso@gmail.com>
2026-05-07 15:40:35 +0100

    Adicionei um novo ficheiro numeros_iguais.py com introdução de novo codigo.
    Atualizei o ficheiro numeros_iguais.py com código de tratamento de erros (try/except) de modo a garantir que o programa não "parta" caso o utilizador escreva algo que não seja um número inteiro (como uma letra).

--- numeros_iguais.py
+++ numeros_iguais.py
@@ -1,16 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May  5 14:16:40 2026
+try:
+    num1 = int(input("Introduz um número inteiro: "))
+    num2 = int(input("Introduz outro número inteiro: "))
 
-@author: Secretaria
-"""
-
-
-num1 = int(input("Introduz um número inteiro: "))
-num2 = int(input("Introduz outro número inteiro: "))
-
-
-if num1 == num2:
-    print("Os números são iguais.")
-else:
-    print("Os números introduzidos são diferentes.")
+    if num1 == num2:
+        print("Os números são iguais.")
+    else:
+        print("Os números introduzidos são diferentes.")
+        
+except ValueError:
+    print("Erro: Por favor, introduz apenas números inteiros (positivos ou negativos).")
